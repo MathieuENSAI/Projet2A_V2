539eebb140c8a33d810b0e59f53800c1db91cbd7
Cyrille-Kmer <cyrillengwem@gmail.com>
2024-10-21 16:09:26 +0200

    update class Movie

--- src/Service/MovieService.py
+++ src/Service/MovieService.py
@@ -6,6 +6,7 @@ class MovieService:
 
     def __init__(self, movie_db: None):
         self.movie_db = movie_db
+        
 
     def get_by_id(self, movie_id: int) -> Movie:
         return Movie(id=1, original_title="A Clockwork Orange")
