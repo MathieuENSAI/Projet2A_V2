28c1620759641b9243f6c6db4c5b9b2580f0edde
Cyrille-Kmer <cyrillengwem@gmail.com>
2024-10-21 17:07:31 +0200

    last commit from Yatoute and Alain for today

---
+++ src/DAO/MovieDAO.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from DBConnector import DBConnector
+from src.Model.Movie import Movie
+
+class  MovieDAO:
+    db_connector : DBConnector
+
+    def __init__(self, db_connector: DBConnector):
+
+        self.db_connector = db_connector
+    
+    def get_by_id(self, id_movie:int):
+
+        res = self.db_connector.sql_query("SELECT * FROM Movie WHERE id_movie = %s", (id_movie), "one")
+        
--- src/Model/Movie.py
+++ src/Model/Movie.py
@@ -4,13 +4,31 @@ from datetime import date
 class Movie(BaseModel):
     id_movie: int
     original_title: str
-    data_sortie: date
+    date_sortie: date
     realisateur: str
     langue_vo: str
     genre: str
     resume: str
-    note_moyenne: str
-    
+    average_rating: str = None
+
+    # def update_average_rating(self):
+
+    def info(self):
+        info = f"""
+        id_movie: {self.id_movie}
+        original_title: {self.original_title}
+        date_sortie: {self.date}
+        realisateur: {self.realisateur}
+        langue_vo: {self.langue_vo}
+        genre: {self.genre}
+        resume: {self.re}
+        average_rating: {self.average_rating}
+        """
+
+        return info
+
+
+
 
 
 
