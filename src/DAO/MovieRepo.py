4ad81917b5693b6c2de45d1fde28dd724a3c031d
Cyrille-Kmer <cyrillengwem@gmail.com>
2024-10-22 12:51:52 +0200

    ...

--- src/DAO/MovieRepo.py
+++ src/DAO/MovieRepo.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from datetime import date
 from src.DAO.DBConnector import DBConnector
 from src.Model.Movie import Movie
 
@@ -9,16 +9,40 @@ class  MovieRepo:
     def __init__(self, db_connector: DBConnector):
 
         self.db_connector = db_connector
+
+    def insert_into_db(self, movie: Movie):
+        raw_created = self.db_connector.sql_query(
+        """INSERT INTO Movie(id, original_language, original_title, release_date, title, overview)
+        """,
+        )
     
     def get_by_id(self, movie_id:int):
-
-        raw_movie = self.db_connector.sql_query("SELECT * FROM Movie WHERE movie_id = %s", (movie_id), "one")
-
+        raw_movie = self.db_connector.sql_query("SELECT * FROM Movie WHERE id = %s;", (movie_id), "one")
         if raw_movie is None :
             return None
         else :
             return Movie(**raw_movie)
 
-    def get_by_title(self, ti)
+    def get_by_title(self, title:str):
+        raws_movie = self.db_connector.sql_query("SELECT * FROM Movie WHERE title = %s OR original_title = %s;", (title, title), "all")
+        if raws_movie is None :
+            return None
+        else :
+            return [Movie(**raw_movie) for raw_movie in raws_movie]
+    
+    def get_by_release_date(self, release_date:date):
+        raws_movie = self.db_connector.sql_query("SELECT * FROM Movie WHERE release_date = %s;", (release_date), "all")
+        if raws_movie is None :
+            return None
+        else :
+            return [Movie(**raw_movie) for raw_movie in raws_movie]
+    
+    def get_lastest_released(self, number:int):
+        raws_movie = self.db_connector.sql_query("SELECT * FROM Movie ORDER BY release_date DESC LIMIT %s;", (number), return_type = "all")
+        if raws_movie is None :
+            return None
+        else :
+            return [Movie(**raw_movie) for raw_movie in raws_movie]
+
 
 
