4444bdab8bc40f771f481c38a2a659c867e9793d
MathieuENSAI <mathiecc@free.fr>
2024-10-22 15:16:16 +0200

    petite correction

--- src/Service/PasswordService.py
+++ src/Service/PasswordService.py
@@ -8,7 +8,7 @@ from src.Model.User import User
 
 def hash_password(password: str, salt: Optional[str] = None) -> str:
     if salt is None:
-        salt = "Pepper"
+        raise Exception("salt not found")
     return hashlib.sha256(salt.encode() + password.encode()).hexdigest()
 
 
@@ -23,10 +23,9 @@ def check_password_strength(password: str):
 
 def validate_username_password(username: str, password: str, user_repo: UserRepo) -> User:
     user_with_username: Optional[User] = user_repo.get_by_username(username=username)
-    ## TODO
-    Guy = UserRepo.get_by_username(username)
-    tested_password = hash_password(password, Guy.salt)
-    if tested_password != Guy(password):
+    user = UserRepo.get_by_username(username)
+    tested_password = hash_password(password, user.salt)
+    if tested_password != user(password):
         raise Exception("Incorrect Password")
     ##J'ai fais ça, jsp si c'est bon (à revoir en fction du reste)
     return user_with_username
--- src/Service/UserService.py
+++ src/Service/UserService.py
@@ -10,12 +10,10 @@ class UserService:
         self.user_repo = user_repo
 
     def create_user(self, username: str, password: str) -> User:
-        if self.user_repo.get_by_username(username) != None :
-            raise Exception("This nickname allready exists")
-        check_password_strength(password)
         salt = create_salt()
         pass_word = hash_password(password, salt)
-        self.user_repo.insert_into_db(username = username, salt = salt, hashed_password=pass_word)
+        user = self.user_repo.insert_into_db(username = username, salt = salt, hashed_password=pass_word)
+        return(user)
 
     def get_user(self, user_id: int) -> User | None:
         return self.user_repo.get_by_id(user_id)
