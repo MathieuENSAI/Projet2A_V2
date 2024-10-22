d8a1f7632ed13053415c0cb326749c386cfba9ae
Emoncel4 <parallelcord@gmail.com>
2024-10-22 16:36:08 +0200

    commit

--- src/Model/SeenMovie.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class SeenMovie(BaseModel):
-    id_seenmovie : int | None
-    id_user : int 
-    id_movie : int 
-    seen : bool 
-    note : int = None 
-    favorite : bool = False
--- src/Model/User.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class User(BaseModel):
-    id_user : int
-    username: str
-    pass_word: str
-    salt: str
