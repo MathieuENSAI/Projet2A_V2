e3d51dc5e4145959a33199ce6814f2ed79eefcc6
Cyrille-Kmer <cyrillengwem@gmail.com>
2024-10-23 11:16:39 +0200

    ...

--- src/Model/User.py
+++ src/Model/User.py
@@ -1,6 +1,5 @@
 from pydantic import BaseModel
-from src.Service.UserService import UserService
-from src.Service.MovieService import MovieService
+
 
 class User(BaseModel):
     id_user : int | None
@@ -12,15 +11,15 @@ class User(BaseModel):
     watchlist : list[int] = []
 
 
-    def follow(self, user):
-        following.append(user(id))
-        return self.following
+    # def follow(self, user):
+    #     following.append(user(id))
+    #     return self.following
     
-    def stop_follow(self, username):
-        ##TODO
+    # def stop_follow(self, username):
+    #     ##TODO
     
-    def add_favorite_movie(self, movie_title):
-        ##TODO
+    # def add_favorite_movie(self, movie_title):
+    #     ##TODO
         
     
 
