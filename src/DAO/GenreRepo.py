d128ad8404dee1cdcbf7e7c8944e74d4c284b2a3
Cyrille-Kmer <cyrillengwem@gmail.com>
2024-10-23 14:18:23 +0200

    ...

--- src/DAO/GenreRepo.py
+++ src/DAO/GenreRepo.py
@@ -8,19 +8,22 @@ class  GenreRepo:
 
         self.db_connector = db_connector
 
-    def insert_into_db(self, genres: list[Genre]):
-
-        query = "INSERT INTO Genre(id_genre, name_genre) VALUES (%s, %s)"
-        if len(genres)>1 :
-            query += ", (%s, %s)"*(len(genres)-1)
+    def insert_into_db(self, **genres):
+        # Vérification de la présence de données
+        if not genres:
+            return None
+        query = "INSERT INTO Movie(id_genre, id_name) VALUES (%s, %s)"
+        if len(genres) > 1:
+            query += ", (%s, %s)" * (len(genres) - 1)
         query += " RETURNING *;"
-        data = tuple(attribut for genre in genres for attribut in (
-            genre.id_genre, genre.name_genre
-            ))
+
+        data = tuple(attribut for genre in genres.values() for attribut in (genre['id'], genre['name']))
 
         raw_created = self.db_connector.sql_query(query, data, "all")
-        if raw_created is None :
+
+        if raw_created is None:
             return None
-        else :
-            return genres
+        else:
+            return [Genre(**attributs) for attributs in raw_created]
+
   
