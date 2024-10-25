d214bf60295fa2e6b40b0d2ddb41f556deb82def
Cyrille-Kmer <cyrillengwem@gmail.com>
2024-10-25 19:45:13 +0200

    ...

---
+++ brouillons/brouillon.py
@@ -0,0 +1,2 @@
+mon_dictionnaire = {'a':1, 'b':5, 'd':3, 'c':10}
+print(dict(list(mon_dictionnaire.items())[:3]))
--- src/Service/MovieFromTMDService.py
+++ src/Service/MovieFromTMDService.py
@@ -15,18 +15,18 @@ class MovieFromTMDService:
 
     def __init__(self, movie_db: None = None):
         self.movie_db = movie_db
-        
-
-    def get_by_id(self, movie_id: int) -> Movie:
-        url = f'https://api.themoviedb.org/3/movie/{movie_id}'
-        response = requests.get(url,headers=self.header)
-        data = response.json()
-        return Movie(id = movie_id, original_language = data.get('original_language', None),
+    
+    def build_movie(self, data:dict) -> Movie:
+        return Movie(id = data['id'], original_language = data.get('original_language', None),
                      original_title = data.get('original_title', None),
                      release_date = data.get('release_date', None),
                      title = data.get('title', None),
                      overview = data.get('overview', None))
 
+    def get_by_id(self, movie_id: int) -> Movie:
+        url = f'https://api.themoviedb.org/3/movie/{movie_id}'
+        film = requests.get(url,headers=self.header).json()
+        return self.build_movie(film) if len(film)>0 else None
 
     def get_by_title(self, title: str):
         payload = {'query': title}
@@ -34,18 +34,7 @@ class MovieFromTMDService:
         params=payload, headers=self.header)
         data = response.json()
         films = data.get('results', [])
-        resultats = []
-        for film in films:
-            movie = Movie(
-                id=film['id'],
-                original_language=film.get('original_language',None),
-                original_title=film.get('original_title',None),
-                release_date=film.get('release_date',None),
-                title=film.get('title',None),
-                overview=film.get('overview',None)
-            )
-            resultats.append(movie)
-        return resultats
+        return [self.build_movie(film) for film in films] if len(films)>0 else None
     
     def get_by_release_period(self, start_release_date: str, end_release_date: str):
         payload = {'primary_release_date.gte': start_release_date,
@@ -54,18 +43,7 @@ class MovieFromTMDService:
                                 params=payload, headers=self.header)
         data = response.json()
         films = data.get('results', [])
-        resultats = []
-        for film in films:
-            movie = Movie(
-                id=film['id'],
-                original_language=film.get('original_language', None),
-                original_title=film.get('original_title', None),
-                release_date=film.get('release_date', None),
-                title=film.get('title', None),
-                overview=film.get('overview', None)
-            )
-            resultats.append(movie)
-        return resultats
+        return [self.build_movie(film) for film in films] if len(films)>0 else None
 
     def search_movie(self, query: str,
                         language = None, primary_release_year = None, 
@@ -77,20 +55,18 @@ class MovieFromTMDService:
                     'year': year}
         response = requests.get('https://api.themoviedb.org/3/search/movie?',
         params=payload, headers=self.header)
+        fimls = response.json()
+        return [self.build_movie(film) for film in films] if len(films)>0 else None
+
+    def get_lastest_released(self, number:int)-> list[Movie]:
+        payload = {"sort_by" : "primary_release_date.desc"}
+        response = requests.get('https://api.themoviedb.org/3/discover/movie?', 
+                                params=payload, headers=self.header)
         data = response.json()
         films = data.get('results', [])
-        resultats = []
-        for film in films:
-            movie = Movie(
-                id=film['id'],
-                original_language=film.get('original_language', None),
-                original_title=film.get('original_title', None),
-                release_date=film.get('release_date', None),
-                title=film.get('title', None),
-                overview=film.get('overview', None)
-            )
-            resultats.append(movie)
-        return resultats if len(resultats)>0 else None
+        films = dict(list(films.items())[:number])
+        return [self.build_movie(film) for film in films] if len(films)>0 else None
+        
 
     def get_id_name_genre(self)-> list[Genre]:
         url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
