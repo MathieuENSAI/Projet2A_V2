4203e5b6ce4e5109042fe4ce6797ef1abfeff436
Yatoute <mintoamayatoute@gmail.com>
2024-11-15 13:55:46 +0100

    ...

--- tests/DAO/test_SeenMovieRepo.py
+++ tests/DAO/test_SeenMovieRepo.py
@@ -421,4 +421,3 @@ def test_get_list_users_by_movie():
     assert users[1] == User(id_user=5, username="",pass_word="", salt=None)
     assert users[2] == User(id_user=4, username="",pass_word="", salt=None)
 
-def test
