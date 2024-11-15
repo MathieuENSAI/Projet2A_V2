49a54fc6ad1c0c6ee23d25a31d2334828aab7ec5
Yatoute <mintoamayatoute@gmail.com>
2024-11-15 14:22:03 +0100

    ....

--- src/App/FollowingController.py
+++ src/App/FollowingController.py
@@ -16,3 +16,7 @@ def add_following(following_id: int, credentials: Annotated[HTTPAuthorizationCre
     if following is None:
         raise HTTPException(status_code=404, detail=f"You can not follow user with id[{following_id}]. check and try adain")
     return {"following":following} |following_service.get_following_movies_collection(user_id, following_id)
+
+@following_route.get("/following-movies-collection",  status_code=status.HTTP_200_OK, dependencies=[Depends(JWTBearer())])
+def get_following_movies_collection(following_id: int, credentials: Annotated[HTTPAuthorizationCredentials, Depends(JWTBearer())]):
+    user_id = jwt_service.validate_user_jwt(credentials.credentials)
