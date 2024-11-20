70eed4372639807e687dcff0ec86bd6c5a170538
Yatoute <mintoamayatoute@gmail.com>
2024-11-20 09:08:13 +0100

    ...

--- src/App/API.py
+++ src/App/API.py
@@ -9,7 +9,14 @@ from .init_app import scheduler_service
 
 
 def run_app():
-    app = FastAPI(title="Réseau social ciné", description="Réseau social ciné")
+    app = FastAPI(title="Réseau social ciné", 
+                description="""<p>
+                Réseau social Cinéma is a web service that allows users to interact with their movie preferences and explore films through social connections.
+                Users can create an account, add movies to their favorites and watchlists, rate movies, and track their viewing history.
+                The system also supports following other users, viewing their movie collections, and discovering films that have been watched by both the user and those they follow. 
+                Authenticated users have access to personalized and detailed data, including the ability to rate movies and manage their collections. 
+                Users can receive suggestions for new follows, and the platform ensures that only valid, authenticated users can perform actions like rating or following. Additionally, the project integrates social features, allowing users to engage with others' movie choices, share collections, and collaborate on discovering new films together.
+                </p>""")
 
     app.include_router(user_router)
 
--- src/App/MovieController.py
+++ src/App/MovieController.py
@@ -10,8 +10,11 @@ from datetime import date
 
 movie_router = APIRouter(prefix="/movies", tags=["Movies"])
 
-@movie_router.get("/{movie_id}", status_code=status.HTTP_200_OK)
+@movie_router.get("/{movie_id}", status_code=status.HTTP_200_OK, summary="Search for a movie by Id")
 def get_movie_by_id(movie_id: int, credentials: Annotated[HTTPAuthorizationCredentials, Depends(JWTBearer(partial_access_allowed=True))]):
+    """ 
+    Search for a movie using its unique identifier. Basic information is accessible to all users, while authenticated users with a valid token can access additional details.
+    """
     try:
         user_id = jwt_service.validate_user_jwt(credentials.credentials)  
     except Exception :
@@ -28,8 +31,11 @@ def get_movie_by_id(movie_id: int, credentials: Annotated[HTTPAuthorizationCrede
         logging.error(f"Error occurred: {e}")
         raise HTTPException(status_code=400, detail=f"Invalid request: {e}") from e
 
-@movie_router.get("/search/title/{title}", status_code=status.HTTP_200_OK)
+@movie_router.get("/search/title/{title}", status_code=status.HTTP_200_OK, summary="Search for a movie by title")
 def get_movie_by_title(title: str, credentials: Annotated[HTTPAuthorizationCredentials, Depends(JWTBearer(partial_access_allowed=True))]):
+    """ 
+     Find movies by providing their title. Unauthenticated users can access basic details, while authenticated users with a valid token can view more comprehensive information.
+    """
     try:
         user_id = jwt_service.validate_user_jwt(credentials.credentials)  
     except Exception :
@@ -41,8 +47,11 @@ def get_movie_by_title(title: str, credentials: Annotated[HTTPAuthorizationCrede
         logging.error(f"Error occurred: {e}")
         raise HTTPException(status_code=400, detail=f"Invalid request: {e}") from e
 
-@movie_router.get("/search/genre/{genre}", status_code=status.HTTP_200_OK)
+@movie_router.get("/search/genre/{genre}", status_code=status.HTTP_200_OK, summary="Search for movies by genre")
 def get_movie_by_genre(genre: str, credentials: Annotated[HTTPAuthorizationCredentials, Depends(JWTBearer(partial_access_allowed=True))]):
+    """ 
+    This route lets you explore movies within a specific genre. Partial access is available without authentication, providing basic information about the movies. However, to view more detailed information, a valid authentication token is required.
+    """
     try:
         user_id = jwt_service.validate_user_jwt(credentials.credentials)  
     except Exception :
@@ -58,8 +67,11 @@ class ReleasePeriod(BaseModel):
     start: date
     end: date 
 
-@movie_router.get("/search/release_period", status_code=status.HTTP_200_OK)
+@movie_router.get("/search/release_period", status_code=status.HTTP_200_OK, summary="Search for movies by release period")
 def get_movie_by_release_period(release_period:ReleasePeriod=Depends(ReleasePeriod)):
+    """ 
+    This route allows you to search for movies within a specified release period. By providing a start and end date, you can retrieve movies released within that timeframe.
+    """
     try:
         movies = movie_service.get_by_release_period(release_period.start, release_period.end)
         return movies
@@ -67,8 +79,11 @@ def get_movie_by_release_period(release_period:ReleasePeriod=Depends(ReleasePeri
         logging.error(f"Error occurred: {e}")
         raise HTTPException(status_code=400, detail=f"Invalid request: {e}") from e
 
-@movie_router.get("/search/lastest_released/{number}", status_code=status.HTTP_200_OK)
+@movie_router.get("/search/lastest_released/{number}", status_code=status.HTTP_200_OK, summary="Retrieve the latest released movies")
 def get_lastest_released(number:int):
+    """ 
+    This endpoint retrieves the most recently released movies. You can specify the number of movies you want to receive by providing the number parameter in the URL. The movies are returned in descending order of their release date, ensuring you get the latest releases first.
+    """
     try:
         movies = movie_service.get_lastest_released(number)
         return movies
